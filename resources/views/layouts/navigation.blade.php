9efbba6714ebd6afad1ab8da5b5c6610a100407c
fanarkt08 <fanarkt08@gmail.com>
2026-04-15 16:05:21 +0200

    correct activity 8 - permission

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                         {{ __('Produits') }}
                     </x-nav-link>
 
-                    @role('admin')
+                    @can('view dashboard')
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
@@ -84,7 +84,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            @role('admin')
+            @can('view dashboard')
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
                 </x-responsive-nav-link>
